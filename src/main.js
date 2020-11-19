f036f3492c836c4215137eb16240b97d90db36cd
taylulz <taylulzcode@gmail.com>
2020-11-19 12:12:53 -0800

    save progress working api calls and styling
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- src/main.js
+++ src/main.js
@@ -8,7 +8,14 @@ import ImgSearch from './img-search.js';
 
 function getElements(response) {
   if (response[698]){
-    $('#atmosphericTemp').text(`The atmospheric temperature on Mars is ${response[698].AT.av} degrees Something?`);
+    $('#sol').html(`Sol #${response.sol_keys[1]}`);
+    $('#atmosphericTemp').html(`The average for today is: ${response[698].AT.av}°?`);
+    $('#lowTemp').html(`The low for today is: ${response[698].AT.mn}°?`);
+    $('#highTemp').html(`The high for today is: ${response[698].AT.mx}°?`);
+    $('#sol1').html(`Sol #${response.sol_keys[2]}`);
+    $('#atmosphericTemp1').html(`The average for today is: ${response[699].AT.av}°?`);
+    $('#lowTemp1').html(`The low for today is: ${response[699].AT.mn}°?`);
+    $('#highTemp1').html(`The high for today is: ${response[699].AT.mx}°?`);
   } else if (response.photos) {
     $('#marsImage').append(`<img src="${response.photos[15].img_src}">`);
   } else if (response.collection.items) {
@@ -36,29 +43,28 @@ async function makeSearchCall(search) {
   getElements(response);
 }
 
-async function makeBackgroundCall(search) {
-  const response = await ImgSearch.getImgSearch(search);
-  background(response);
-  console.log("call");
-}
+// async function makeBackgroundCall(search) {
+//   const response = await ImgSearch.getImgSearch(search);
+//   background(response);
+//   console.log("call");
+// }
 
-function background(response) {
-  let random = Math.floor((Math.random()*10)+1);
-  let randomUrl = `"url('${response.collection.items[random].links[0].href}')"`;
-  document.body.style.backgroundImage = randomUrl;
-  console.log(random);
-  console.log(randomUrl);
-}
+// function background(response) {
+//   let random = Math.floor((Math.random()*10)+1);
+//   let randomUrl = `"url('${response.collection.items[random].links[0].href}')"`;
+//   document.body.style.backgroundImage = randomUrl;
+//   console.log(random);
+//   console.log(randomUrl);
+// }
 
 $(document).ready(function() {
-  makeBackgroundCall("earth");
-  console.log(makeBackgroundCall("earth"));
+  // makeBackgroundCall("earth");
+  // console.log(makeBackgroundCall("earth"));
   makeApiCall();
   makeImgCall();
   $('#searchClick').click(function(){
     let search = $('#userSeach').val();
     $('#imgSearchResults').empty();
     makeSearchCall(search);
-    console.log(makeSearchCall(search));
   });
 });
